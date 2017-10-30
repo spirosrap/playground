797fb84b39000c34f89c603ceb84fb399af4bb67
Spiros Raptis <spirosrap@gmail.com>
2017-10-30 16:57:07 +0200

    runnable script

--- sieve.py
+++ sieve.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+
 from math import ceil,sqrt
+import sys
+
 def sieve(n):
 	A = [True]*(n + 1)
 	for i in range(2,ceil(sqrt(n)+1)):
@@ -8,4 +12,9 @@ def sieve(n):
 	return([i for i in range(len(A)) if A[i]== True][2:])
 
 
-print(sieve(100))
+if __name__ == "__main__":
+    a = int(sys.argv[1])
+    try:
+    	print(sieve(a))
+    except:
+    	print("Please use the script like this \"./sieve 100")
